5f96d5982b2b5fa5c8d360a660070ccece201aa5
srssyd <shanyd15@mails.tsinghua.edu.cn>
2015-09-24 02:55:02 +0800

    add make file

--- auto_sync.sh
+++ auto_sync.sh
@@ -3,7 +3,7 @@ for port in `cat port.list`
 do
 ssh -t -t root@zhaoshang -p $port "
 cd /home/sso ;
-ps -ef | grep server | awk '{print $2}' | xargs kill -9;
+#ps -ef | grep server | awk '{print $2}' | xargs kill -9;
 git pull
 make clean;
 make server;
@@ -15,7 +15,7 @@ for port in `cat port_python.list`
 do
 ssh -t -t root@zhaoshang -p $port "
 cd /root/sso ;
-ps -ef | grep python | awk '{print $2}' | xargs kill -9;
+#ps -ef | grep python | awk '{print $2}' | xargs kill -9;
 git pull;
 cd flask-proxy/project;
 python runserver.py runserver;
