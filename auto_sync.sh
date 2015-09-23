5f61c735a7ade74c87a9809dae62642ae3a7a7a9
srssyd <shanyd15@mails.tsinghua.edu.cn>
2015-09-24 02:49:44 +0800

    Fix bug

--- auto_sync.sh
+++ auto_sync.sh
@@ -3,7 +3,7 @@ for port in `cat port.list`
 do
 ssh -t -t root@zhaoshang -p $port "
 cd /home/sso ;
-ps -ef | grep firefox | awk '{print $2}' | xargs kill -9 kill;
+ps -ef | grep server | awk '{print $2}' | xargs kill -9;
 git pull
 make clean;
 make server;
@@ -11,3 +11,13 @@ make server;
 " 
 done
 
+for port in `cat port_python.list` 
+do
+ssh -t -t root@zhaoshang -p $port "
+cd /root/sso ;
+ps -ef | grep python | awk '{print $2}' | xargs kill -9;
+git pull;
+cd flask-proxy/project;
+python runserver.py runserver;
+" 
+done
