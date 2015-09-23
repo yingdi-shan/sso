89028b745f91e66a7c38980b8b7c223fd4baa081
srssyd <shanyd15@mails.tsinghua.edu.cn>
2015-09-24 03:52:18 +0800

    Add benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -16,18 +16,19 @@ using namespace std;
 atomic<int> x;
 atomic<int> failtime;
 
-char* hostname; 
+char* hostname;
 int testtime;
 int mod;
 
 vector<pair<string, string> > userlist;
 
-void remote_login(){
+void remote_login(const string& user_id,const string& password){
     using namespace boost::network;
     using namespace boost::network::http;
 
     try{
-        client::request request_(hostname);
+	string req = hostname+"/login?username="+user_id+"&password="+password;
+        client::request request_(req);
         request_ << header("Connection", "close");
         client client_;
         client::response response_ = client_.get(request_);
@@ -78,29 +79,13 @@ void verify(){
     }
 
 }
-void login(){
+void add_user(const string &username,const string &pwd){
     using namespace boost::network;
     using namespace boost::network::http;
 
     try{
-        client::request request_(hostname);
-        request_ << header("Connection", "close");
-        client client_;
-        client::response response_ = client_.get(request_);
-        std::string body_ = body(response_);
-        int fault_time=0;
-        x++;
-    }catch( std::exception & e){
-        failtime++;
-        cout << "exception" << endl;
-    }
-}
-void add_user(){
-    using namespace boost::network;
-    using namespace boost::network::http;
-
-    try{
-        client::request request_(hostname);
+        string req = hostname+"/add_user?username="+username+"&password="+pwd;
+        client::request request_(req);
         request_ << header("Connection", "close");
         client client_;
         client::response response_ = client_.get(request_);
@@ -113,13 +98,14 @@ void add_user(){
     }
 }
 
-void threadMain(){
+userlist userlists[3000];
+void threadMain(int id){
     auto Start = std::chrono::steady_clock::now( );
-
-    while(std::chrono::steady_clock::now() < Start + std::chrono::milliseconds( testtime )){
+    int cnt=0;
+    while(std::chrono::steady_clock::now() < Start + std::chrono::milliseconds( testtime ) && cnt<userlists[id].size()){
         switch(mod){
             case 0:
-                add_user();
+                add_user(userlists[id].first,userlists[id].second);
                 break;
             case 1:
                 login();
@@ -131,25 +117,28 @@ void threadMain(){
                 update();
                 break;
             case 4:
-                remote_login();
+                remote_login(userlists[id].first,userlists[id].second);
                 break;
             default:
                 return;
                 break;
         }
-        
+
     }
 }
-
 
 int main(int argc, char** argv){
         fstream fin("data/http.txt");
         string line;
+
+        int numThreads=atoi(argv[2]);
+
+        int cnt=0;
         while(getline(fin, line)){
             cout << line << endl;
             string username = line.substr(0, 8);
             string pwd = line.substr(9, 17);
-            userlist.push_back(pair<string, string>(username, pwd));
+            userlists[cnt++%].push_back(pair<string, string>(username, pwd));
         }
 
 
@@ -159,8 +148,8 @@ int main(int argc, char** argv){
             hostname = argv[1];
             cout << hostname << endl;
         }
-/*    
-        int numThreads=atoi(argv[2]);
+
+
         testtime = atoi(argv[3]);
         mod = atoi(argv[4]);
 
@@ -168,12 +157,12 @@ int main(int argc, char** argv){
 
         using namespace std::chrono;
         steady_clock::time_point t1 = steady_clock::now();
-        cout << "start " << endl; 
+        cout << "start " << endl;
 
         for (int i = 0; i < numThreads; i++) {
-            threads.push_back(std::thread(threadMain));
+            threads.push_back(std::thread(threadMain,i));
         }
-        
+
         for (int i = 0; i < numThreads; i++) {
             threads[i].join();
         }
@@ -182,7 +171,7 @@ int main(int argc, char** argv){
         duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
         cout << "time: " << time_span.count() <<endl;
         cout << "requests " << x << endl;
- */
+
 
     return 0;
 }
